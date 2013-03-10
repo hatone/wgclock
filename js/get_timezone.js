b2cf52c4e581bd3587f1ba55acd445e92723070f
hatone <b1006006@gmail.com>
2013-03-09 19:18:41 -0800

    using local strage

--- js/get_timezone.js
+++ js/get_timezone.js
@@ -1,8 +1,19 @@
+var currentTimezone;
+var myTimezone;
+
 function getMyTimezone(){
   httpObj = new XMLHttpRequest();
   httpObj.open("get", "./js/seeds/timezone_list.json", true);
   httpObj.onload = function(){
+
     var myData = JSON.parse(this.responseText);
+
+    //var myTimezone = JSON.parse(localStorage.my_timezone);
+    //console.log('show my_timezone' + myTimezone.name);
+    //console.log('set my_timezone:' + myData.timezone[2].name);
+    //localStorage.setItem("myTimezone", JSON.stringify(myData.timezone));
+    //localStorage.setItem("currentTimezone", JSON.stringify(myData.timezone[2]));
+
     var txt = "";
     for (var i=0; i<myData.timezone.length; i++){
       txt = txt + myData.timezone[i].name + " is " + myData.timezone[i].difference+" difference<br>";
@@ -11,6 +22,8 @@ function getMyTimezone(){
   }
   httpObj.send(null);
 }
+
+
 
 
 
