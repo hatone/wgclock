3348038aaec1b4e33ab97c9b2519a0ddb49651f0
hatone <b1006006@gmail.com>
2013-03-09 23:59:13 -0800

    be able to read and calcurate

--- js/get_timezone.js
+++ js/get_timezone.js
@@ -1,6 +1,3 @@
-var currentTimezone;
-var myTimezone;
-
 function getMyTimezone(){
   httpObj = new XMLHttpRequest();
   httpObj.open("get", "./js/seeds/timezone_list.json", true);
@@ -8,22 +5,48 @@ function getMyTimezone(){
 
     var myData = JSON.parse(this.responseText);
 
-    //var myTimezone = JSON.parse(localStorage.my_timezone);
-    //console.log('show my_timezone' + myTimezone.name);
-    //console.log('set my_timezone:' + myData.timezone[2].name);
-    //localStorage.setItem("myTimezone", JSON.stringify(myData.timezone));
-    //localStorage.setItem("currentTimezone", JSON.stringify(myData.timezone[2]));
+    var myTimezones = JSON.parse(localStorage.myTimezone);
+    var currentTimezone = JSON.parse(localStorage.currentTimezone);
 
-    var txt = "";
-    for (var i=0; i<myData.timezone.length; i++){
-      txt = txt + myData.timezone[i].name + " is " + myData.timezone[i].difference+" difference<br>";
-    }
-    document.getElementById("result").innerHTML = txt;
+    //var txt = "";
+    //for (var i=0; i<myData.timezone.length; i++){
+    //  txt = txt + myData.timezone[i].name + " is " + myData.timezone[i].difference+" difference<br>";
+    //}
+    //document.getElementById("result").innerHTML = txt;
+    showMyTimes(currentTimezone,myTimezones);
   }
   httpObj.send(null);
 }
 
+function showMyTimes(currentTimezone,myTimezones){
+    console.log(myTimezones);
+    console.log(currentTimezone);
 
+    var current_str = "";
+    plotTime(0,currentTimezone.name);
 
+    console.log(current_str);
+
+    for (var i=0; i < myTimezones.length; i++){ 
+      var diff = parseInt(myTimezones[i].difference) - parseInt(currentTimezone.difference);
+      console.log(diff);
+      plotTime(diff,myTimezones[i].name)
+    }
+}
+
+function plotTime(diff,name){
+      var myTimezoneStr = '<div class="btn-group"><div class="btn btn-danger timezone">'+ name + '</div>';
+      for (var i=0; i<=23; i++){
+        var t = i + diff;
+        if (t > 23) t = -24 + t;
+        if (t >= 8 && t <= 22){
+          myTimezoneStr += '<div class="btn btn-primary">' + t + "</div>";
+        } else {
+          myTimezoneStr += '<div class="btn">'+ t + "</div>";
+        }
+      }
+      myTimezoneStr += "</div></br></br>";
+      document.getElementById("my_timezones").innerHTML += myTimezoneStr;
+}
 
 
