6129ce58f79c67abaa0b2237eb78dcccce868261
hatone <b1006006@gmail.com>
2016-02-04 22:32:42 -0800

    add JST

--- js/get_timezone_list.js
+++ js/get_timezone_list.js
@@ -1,93 +1,93 @@
 function getTimezoneList(){
-  httpObj = new XMLHttpRequest();
-  httpObj.open("get", "./js/seeds/timezone_list.json", true);
-  httpObj.onload = function(){
+    httpObj = new XMLHttpRequest();
+    httpObj.open("get", "./js/seeds/timezone_list.json", true);
+    httpObj.onload = function(){
 
-    var timezoneList = JSON.parse(this.responseText);
+        var timezoneList = JSON.parse(this.responseText);
 
-    var currentTimezoneOption = "";
-    var currentTimezone = "";
-    
-    if(localStorage.currentTimezone != null) {
-      currentTimezone = JSON.parse(localStorage.currentTimezone);
+        var currentTimezoneOption = "";
+        var currentTimezone = "";
 
-      timezoneList.timezone.forEach(function(tz,index){
-        if (currentTimezone.name == tz.name) {
-          currentTimezoneOption = currentTimezoneOption + '<option value="'+index +'" selected="selected">' + tz.name + " (" + tz.difference + ")</option>";
-        }
-        else {
-          currentTimezoneOption = currentTimezoneOption + '<option value="'+ index +'">' + tz.name + " (" + tz.difference + ")</option>";
-        }
-      });
-    } else {
-      timezoneList.timezone.forEach(function(tz,index){
-        currentTimezoneOption = currentTimezoneOption + '<option value="'+ index +'">' + tz.name + " (" + tz.difference + ")</option>";
-      });
-    }
+        if(localStorage.currentTimezone != null) {
+            currentTimezone = JSON.parse(localStorage.currentTimezone);
 
-    var myTimezoneOptions = "";
-    if(localStorage.myTimezone != null) {
-      var myTimezones = JSON.parse(localStorage.myTimezone);
-        myTimezones.forEach(function(ltz) {
-            timezoneList.timezone.forEach(function(tz,index) {
-                if (ltz.name == tz.name) {
-                    myTimezoneOptions = myTimezoneOptions + '<option value="'+ index +'" selected="selected">' + tz.name + " (" + tz.difference + ")</option>";
+            timezoneList.timezone.forEach(function(tz,index){
+                if (currentTimezone.name == tz.name) {
+                    currentTimezoneOption = currentTimezoneOption + '<option value="'+index +'" selected="selected">' + tz.name + " (" + tz.difference + ")</option>";
                 }
                 else {
-                    myTimezoneOptions = myTimezoneOptions + '<option value="'+ index +'">' + tz.name + " (" + tz.difference + ")</option>";
-                }  
+                    currentTimezoneOption = currentTimezoneOption + '<option value="'+ index +'">' + tz.name + " (" + tz.difference + ")</option>";
+                }
             });
-            $('#timezone_forms').append('<br>');
-            $('#timezone_forms').append('<select class="timezones" id="my_timezones_menu2">' + myTimezoneOptions + '</select>');
-            myTimezoneOptions = "";
-        });
-    } else {
-      timezoneList.timezone.forEach(function(tz,index) {
-        myTimezoneOptions = myTimezoneOptions + '<option value="'+ index +'">' + tz.name + " (" + tz.difference + ")</option>";
-      });
-    }
+        } else {
+            timezoneList.timezone.forEach(function(tz,index){
+                currentTimezoneOption = currentTimezoneOption + '<option value="'+ index +'">' + tz.name + " (" + tz.difference + ")</option>";
+            });
+        }
 
-    document.getElementById("current_timezone").innerHTML = currentTimezoneOption
-  }
-  httpObj.send(null);
+        var myTimezoneOptions = "";
+        if(localStorage.myTimezone != null) {
+            var myTimezones = JSON.parse(localStorage.myTimezone);
+            myTimezones.forEach(function(ltz) {
+                timezoneList.timezone.forEach(function(tz,index) {
+                    if (ltz.name == tz.name) {
+                        myTimezoneOptions = myTimezoneOptions + '<option value="'+ index +'" selected="selected">' + tz.name + " (" + tz.difference + ")</option>";
+                    }
+                    else {
+                        myTimezoneOptions = myTimezoneOptions + '<option value="'+ index +'">' + tz.name + " (" + tz.difference + ")</option>";
+                    }
+                });
+                $('#timezone_forms').append('<br>');
+                $('#timezone_forms').append('<select class="timezones" id="my_timezones_menu2">' + myTimezoneOptions + '</select>');
+                myTimezoneOptions = "";
+            });
+        } else {
+            timezoneList.timezone.forEach(function(tz,index) {
+                myTimezoneOptions = myTimezoneOptions + '<option value="'+ index +'">' + tz.name + " (" + tz.difference + ")</option>";
+            });
+        }
+
+        document.getElementById("current_timezone").innerHTML = currentTimezoneOption
+    }
+    httpObj.send(null);
 }
 
 function addTimezone(){
-  httpObj = new XMLHttpRequest();
-  httpObj.open("get", "./js/seeds/timezone_list.json", true);
-  httpObj.onload = function(){
+    httpObj = new XMLHttpRequest();
+    httpObj.open("get", "./js/seeds/timezone_list.json", true);
+    httpObj.onload = function(){
 
-    var myData = JSON.parse(this.responseText);
+        var myData = JSON.parse(this.responseText);
 
-    var txt = "";
-    myData.timezone.forEach(function(tz,index) {
-      txt = txt + '<option value="'+ index +'">' + tz.name + " (" + tz.difference + ")</option>";
-    });
-      
-    $("#timezone_forms").append('<br>');
-    $("#timezone_forms").append('<select class="timezones">' + txt +'</select>');
-  }
-  httpObj.send(null);
+        var txt = "";
+        myData.timezone.forEach(function(tz,index) {
+            txt = txt + '<option value="'+ index +'">' + tz.name + " (" + tz.difference + ")</option>";
+        });
+
+        $("#timezone_forms").append('<br>');
+        $("#timezone_forms").append('<select class="timezones">' + txt +'</select>');
+    }
+    httpObj.send(null);
 }
 
 function submitTimezone(){
-  httpObj = new XMLHttpRequest();
-  httpObj.open("get", "./js/seeds/timezone_list.json", true);
-  var timezoneList;
-  httpObj.onload = function(){
-    timezoneList = JSON.parse(this.responseText);
+    httpObj = new XMLHttpRequest();
+    httpObj.open("get", "./js/seeds/timezone_list.json", true);
+    var timezoneList;
+    httpObj.onload = function(){
+        timezoneList = JSON.parse(this.responseText);
 
-    var timezones = new Array();
-    $('.timezones').each(function(index,e) { 
-      timezones.push(timezoneList.timezone[e.value]);
-    });
+        var timezones = new Array();
+        $('.timezones').each(function(index,e) {
+            timezones.push(timezoneList.timezone[e.value]);
+        });
 
-    localStorage.setItem("currentTimezone",JSON.stringify(timezones[0]));
-    timezones.shift();
-    localStorage.setItem("myTimezone",JSON.stringify(timezones));
-    document.getElementById("my_timezones").innerHTML = '';
-    getMyTimezone();
-  }
-  httpObj.send(null);
+        localStorage.setItem("currentTimezone",JSON.stringify(timezones[0]));
+        timezones.shift();
+        localStorage.setItem("myTimezone",JSON.stringify(timezones));
+        document.getElementById("my_timezones").innerHTML = '';
+        getMyTimezone();
+    }
+    httpObj.send(null);
 }
 
