a50d5af31e81923bca88a22f50c2851f66deaae3
hatone <b1006006@gmail.com>
2016-02-07 16:47:51 -0800

    change file names

--- js/wgclock.js
+++ js/wgclock.js
@@ -1,3 +1,67 @@
+$(function() {
+    setInterval ( 'clocknow()',1000 );
+    getMyTimezone();
+    getTimezoneList();
+    $('#add_timezone').click(function(){
+        addTimezone();
+    });
+    $("form#setting_form").submit(function() {
+        submitTimezone();
+    });
+    setTimeout(function(){
+        if(localStorage.tutorial == null) {
+            $('body').chardinJs('start')
+            localStorage.setItem("tutorial",'1');
+        }
+    }, 1500);
+
+});
+
+function getMyTimezone(){
+    httpObj = new XMLHttpRequest();
+    httpObj.open("get", "./js/seeds/timezone_list.json", true);
+    httpObj.onload = function(){
+
+        if(localStorage.currentTimezone == null) {
+            localStorage.setItem("currentTimezone",'{"name":"GMT","difference":0}');
+        }
+        if(localStorage.myTimezone == null) {
+            localStorage.setItem("myTimezone",'[{"name":"PST","difference":-8},{"name":"EST","difference":-5},{"name":"JST","difference":9}]');
+        }
+        var currentTimezone = JSON.parse(localStorage.currentTimezone);
+        var myTimezones = JSON.parse(localStorage.myTimezone);
+
+        showMyTimes(currentTimezone,myTimezones);
+    }
+
+    httpObj.send(null);
+}
+
+function showMyTimes(currentTimezone,myTimezones){
+    plotTime(0,currentTimezone.name);
+
+    myTimezones.forEach(function(tz){
+        var diff = parseInt(tz.difference) - parseInt(currentTimezone.difference);
+        plotTime(diff,tz.name);
+    });
+}
+
+function plotTime(diff,name){
+    var myTimezoneStr = '<div class="btn-group"><div class="btn btn-danger timezone">'+ name + '</div>';
+    for (var i=0; i<=23; i++){
+        var t = i + diff;
+        if (t > 23) t = -24 + t;
+        if (t <= -1) t = 24 + t;
+        if ((t >= 8 && t <= 22)){
+            myTimezoneStr += '<div class="btn btn-primary hour">' + t + "</div>";
+        } else {
+            myTimezoneStr += '<div class="btn hour">'+ t + "</div>";
+        }
+    }
+    myTimezoneStr += "</div></br></br>";
+    document.getElementById("my_timezones").innerHTML += myTimezoneStr;
+}
+
 function getTimezoneList(){
     httpObj = new XMLHttpRequest();
     httpObj.open("get", "./js/seeds/timezone_list.json", true);
@@ -79,7 +143,7 @@ function addTimezone(){
         } else {
             index = parseInt(lastobj.id.replace("tzbtn",""))+1;
         }
-            
+
         $('#timezone_forms').append('<br>');
         $('#timezone_forms').append('<select class="timezones" id="tzbtn'+index+'">' + txt + '</select>');
         $('#timezone_forms').append(' <button onclick = "deleteTimezone(event,'+index+');" form = "timezone_forms" id=dbtn'+index+'><span class="fui-cross-16"></span></button>');
@@ -107,4 +171,6 @@ function submitTimezone(){
     }
     httpObj.send(null);
 }
+
+
 
